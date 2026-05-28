e18aa833de8736f66d84027abf61cde7b278c6e5
Charlis <ekaut0010@gmail.com>
2026-05-28 00:29:22 -0400

    1 - Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil,com o método main, que ao executar deverá imprimir um cumprimento personalizado no console. Por exemplo,Olá, (SeuNome!). 2 - Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo,Tudo bem?. 3 -Crie uma classe chamada Estudos para imprimir anotações de estudos no console. 4 - Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5. Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor 15 ao invés da expressão 10+5. 5 - Crie uma classe Subtracao para imprimir o resultado de 10 - 5 no console.

---
+++ Fundamentos-01/src/ex1.java
@@ -0,0 +1,26 @@
+///1 - Crie um projeto Java no IntelliJ que deve conter uma classe chamada Perfil,com o método main, que ao executar deverá imprimir um cumprimento personalizado no console. Por exemplo, "Olá, (SeuNome!)".
+///
+/// 2 - Modifique o programa para imprimir uma segunda mensagem de cumprimento personalizada. Por exemplo, "Tudo bem?".
+///
+/// 3 -Crie uma classe chamada Estudos para imprimir anotações de estudos no console.
+///
+/// 4 - Crie uma classe Soma em Java para imprimir no console o resultado da operação 10 + 5. Certifique-se de que o resultado seja exibido sem o uso de aspas, apresentando o valor "15" ao invés da expressão "10+5".
+///
+/// 5 - Crie uma classe Subtracao para imprimir o resultado de 10 - 5 no console.
+
+public class ex1 {
+    public static void main (String[] args){
+        System.out.println("Olá, Charlis");
+        System.out.println("Tudo bem?");
+        System.out.println("----------------------------");
+
+        System.out.println("Java é uma linguagem de programação orientada a objetos");
+        System.out.println("JVM → executa aplicações Java");
+        System.out.println("----------------------------");
+
+        System.out.println(10 + 5);
+        System.out.println("----------------------------");
+        System.out.println(10-5);
+    }
+
+}
